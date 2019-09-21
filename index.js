5c6ee4d23a027ed5ebc8e3a7b4760ae0b372da67
Mayursinh <mayursiinh@gmail.com>
2019-09-21 14:44:24 +0530

    convert with command

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
 const fs = require('fs')
-
-const code = fs.readFileSync('./tests/singleforEach.js', 'utf8')
+const path = require('path')
 
 const CON = require('./lib/converter')
 
-const converter = new CON(code)
+try {
+        
+    const entryFile = process.argv[2]
+    const outputFile = process.argv[3]
 
-converter.convert().then(convertedCode => {
-    console.log(convertedCode)
-}).catch(err => {
-    console.log(err)
-})
+    const entryPath = path.resolve(__dirname, entryFile)
+    const outputPath = path.resolve(__dirname, outputFile)
 
+
+    const code = fs.readFileSync(entryPath, 'utf8')
+
+    let converter = new CON(code)
+
+    converter.convert()
+        .then(convertedCode => {
+
+            fs.writeFile(outputPath, convertedCode, err => {
+
+                if(err) throw err 
+                console.log(`Output : ${outputPath}`)
+
+            })
+
+        })
+
+} catch(err) {
+    throw err 
+}
